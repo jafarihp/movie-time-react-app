032c9d97c68d0cefc67eb9844dbd8e5a051e86c8
MohammadReza <mohammadreza.jafari.hp@gmail.com>
2023-09-05 20:54:15 +0330

    add api from TMDB and test it!

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
 
 function App() {
-  const [count, setCount] = useState(0);
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
+  const apiTesting = () => {
+    fetchDataFromApi("/movie/popular").then((res) => {
+      console.log(res);
+    });
+  };
 
   return <>Movie and Series</>;
 }
