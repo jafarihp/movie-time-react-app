ab9a85c6e4c7c3253949be5a19ee71194c9eb96c
MohammadReza <mohammadreza.jafari.hp@gmail.com>
2023-09-08 23:26:45 +0330

    add footer with github and linkedin link...

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -73,7 +73,7 @@ const Header = () => {
   return (
     <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
       <ContentWrapper>
-        <div className="logo" onClick={() => navigate("/")}>
+        <div className="logo" onClick={() => navigate("/movie-time-react-app")}>
           <img src={logo} alt="" />
         </div>
         <ul className="menuItems">
